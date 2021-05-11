bdddad586e9abc9cd5e34608f27775c022e310c3
Sandeep K Yaramchitti <ysandeepkumar88@gmail.com>
2021-05-10 22:16:12 -0400

    handle refresh token and add search for music

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,13 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {UseAuth} from './UseAuth'
+import {Container, Form } from 'react-bootstrap'
 
 export const Dashboard = ({code}) => {
 
     const accessToken = UseAuth(code)
-    
+    const [search, setSearch] = useState('');
+
     return (
-        <div>
-            {code}
-        </div>
+        <Container>
+            <Form.Control 
+                type="search"
+                placeholder="Search Songs and Artist"
+                value={search}
+                onChange={e => setSearch(e.target.value)}
+            />
+        </Container>
     )
 }
--- src/UseAuth.js
+++ src/UseAuth.js
@@ -1,22 +1,48 @@
-import {useEffect} from 'react'
+// @ts-nocheck
+import {useEffect, useState} from 'react'
 import axios from 'axios'
 
 export const UseAuth = (code) => {
 
 
-    // const [accessToken, setAccessToken] =  useState();
-    // const [refreshToken, setRefreshToken] = useState();
-    // const [expiresIn, setExpiresIn] = useState();
+    const [accessToken, setAccessToken] =  useState();
+    const [refreshToken, setRefreshToken] = useState();
+    const [expiresIn, setExpiresIn] = useState();
 
     useEffect(() => {
         axios.post('http://localhost:3001/login', {
             code
         }).then(res => {
-            console.log(res.data);
+            setAccessToken(res.data.accessToken);
+            setRefreshToken(res.data.refreshToken);
+            setExpiresIn(res.data.expiresIn);
+            window.history.pushState({}, null, '/');
         }).catch(() => {
             // @ts-ignore
-            // window.location = "/"
+            window.location = "/"
         })
         
     }, [code]);
+
+    useEffect(() => {
+        if (!refreshToken || !expiresIn) return;
+        const interval = setInterval(() =>{
+
+        axios.post('http://localhost:3001/refresh', {
+            refreshToken
+        }).then(res => {
+            setAccessToken(res.data.accessToken);
+            setExpiresIn(res.data.expiresIn);
+        }).catch(() => {
+            // @ts-ignore
+            window.location = "/"
+        })
+
+    }, (expiresIn - 60) * 1000)
+
+    return () => clearInterval(interval)
+        
+    }, [refreshToken, expiresIn]);
+
+    return accessToken
 }
