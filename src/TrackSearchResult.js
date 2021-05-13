b9292c0e882c133c0a1b37ccdfd76ac3ad542a91
Sandeep K Yaramchitti <ysandeepkumar88@gmail.com>
2021-05-12 21:50:34 -0400

    more updates to play the spotify songs

--- src/TrackSearchResult.js
+++ src/TrackSearchResult.js
@@ -1,9 +1,21 @@
 import React from 'react'
 
 export const TrackSearchResult = ({track}) => {
+
+    const handlePlay = () => {
+        
+    }
     return (
-        <div>
-            <h1>{track}</h1>
+        <div 
+            className="d-flex m-2 align-content-center"
+            style={{cursor: 'pointer'}}
+            onClick={handlePlay}
+        >
+            <img src={track.albumUrl} alt={track} style={{height: '64px', width: '64px'}} />
+            <div className="ml-3">
+                <div>{track.title}</div>
+                <div className="text-muted">{track.artist}</div>
+            </div>
         </div>
     )
 }
